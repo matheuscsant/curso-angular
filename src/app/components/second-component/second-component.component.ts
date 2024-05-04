b4d4aa7947f6c4ab51a72056397c2d9c7243513f
Matheus Campanhola <matheuscampanholasantos123@gmail.com>
2024-05-04 17:25:27 -0300

    Interpolação de dados

--- src/app/components/second-component/second-component.component.ts
+++ src/app/components/second-component/second-component.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-second-component',
   templateUrl: './second-component.component.html',
-  styleUrl: './second-component.component.css'
+  styleUrl: './second-component.component.css',
 })
 export class SecondComponentComponent {
-
+  name: string = 'Matheus';
+  age: number = 22;
+  occupation = 'Programmer';
+  hobbies = ['Correr', 'Jogar', 'Outro hobbie'];
+  car = {
+    year: '2019',
+    name: 'Argo',
+  };
 }
