cd980559aca9e629cdc7178b7ca8948a56acb5bd
Matheus Campanhola <matheuscampanholasantos123@gmail.com>
2024-05-04 16:51:20 -0300

    Criando primeiro component Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { SecondComponentComponent } from './components/second-component/second-component.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FirstComponentComponent, SecondComponentComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
