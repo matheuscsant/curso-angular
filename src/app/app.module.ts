e5db86d9e3786e11d1d7dbc31548333425b9da87
Matheus Campanhola <matheuscampanholasantos123@gmail.com>
2024-05-05 18:57:21 -0300

    Diretivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
-import { SecondComponentComponent } from './components/second-component/second-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { SecondComponentComponent } from './components/second-component/second-component.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponentComponent, SecondComponentComponent, ParentDataComponent],
+  declarations: [
+    AppComponent,
+    FirstComponentComponent,
+    SecondComponentComponent,
+    ParentDataComponent,
+    DirectivesComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
