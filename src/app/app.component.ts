a22d7ace9584ad1274dcbcd8f6c9771859bcff12
Matheus Campanhola <matheuscampanholasantos123@gmail.com>
2024-05-05 18:42:53 -0300

    Passando informações entre componentes (Pai -> Filho)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  userName = 'Joaquim';
+  idade = 22;
+
+  userData = {
+    email: 'joaquim@gmail.com',
+    role: 'Admin',
+  };
+
   title = 'curso-angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { SecondComponentComponent } from './components/second-component/second-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponentComponent, SecondComponentComponent],
+  declarations: [AppComponent, FirstComponentComponent, SecondComponentComponent, ParentDataComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
