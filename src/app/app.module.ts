711a5d3dc5d6279c56162d0df2e9487c4a487eb9
Matheus Campanhola <matheuscampanholasantos123@gmail.com>
2024-05-19 09:52:19 -0300

    Loops no Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ConditionalComponent } from './components/conditional/conditional.compo
 import { EventosComponent } from './components/eventos/eventos.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { ChangeNumberComponent } from './components/change-number/change-number.
     EventosComponent,
     EmitterComponent,
     ChangeNumberComponent,
+    ListRenderComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
